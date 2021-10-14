ac3ba1f21487ebfd31109458fa2804b48381abc9
tmvkrpxl0 <ironman466@naver.com>
2021-10-15 08:44:11 +0900

    Deleted unnecessary lines

--- src/main/java/dev/lazurite/toolbox/ToolBox.java
+++ src/main/java/dev/lazurite/toolbox/ToolBox.java
@@ -4,6 +4,7 @@ package dev.lazurite.toolbox;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +12,10 @@ import org.apache.logging.log4j.Logger;
 public class ToolBox {
     public static final String MODID = "toolbox";
     public static final Logger LOGGER = LogManager.getLogger("Transporter");
+
+    public ToolBox(){
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
 
     @SubscribeEvent
     public void setup(final FMLCommonSetupEvent event) {
